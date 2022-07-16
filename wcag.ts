6328844df8dd6f35e3d4ddc99800d36c9f557ac2
Sondre Aasemoen <sondre@eons.io>
2022-07-16 16:57:01 +0200

    Add WCAG 2.0/2.1/2.2 changes to code

--- wcag.ts
+++ wcag.ts
@@ -1,16 +1,20 @@
 import { Color, color } from "./color.ts";
 
-interface Readability {
+export type WCAG = "2.0" | "2.1" | "2.2";
+
+export interface Readability {
+  wcag?: WCAG;
   level?: "AA" | "AAA";
   size?: "normal" | "large";
 }
 
-interface ReadabilityResult extends Readability {
+export interface ReadabilityResult extends Readability {
   score: number;
   pass: boolean;
 }
 
-/** See https://www.w3.org/WAI/WCAG22/quickref/#contrast-minimum for values from WCAG 2.2.
+/**
+ * See https://www.w3.org/WAI/WCAG22/quickref/#contrast-minimum for values from WCAG 2.2.
  *
  * - Level AA
  *   The visual presentation of text and images of text has a contrast ratio of at least 4.5:1
@@ -19,6 +23,8 @@ interface ReadabilityResult extends Readability {
  * - Level AAA
  *   The visual presentation of text and images of text has a contrast ratio of at least 7:1
  *   Large Text: Large-scale text and images of large-scale text have a contrast ratio of at least 4.5:1;
+ *
+ * Note: this function is identical for WCAG 2.0, 2.1 and 2.2;
  */
 const contrastLevel = ({ level = "AAA", size = "normal" }: Readability): number => {
   if (level == "AAA" && size === "normal") return 7;
@@ -29,40 +35,82 @@ const contrastLevel = ({ level = "AAA", size = "normal" }: Readability): number 
 
 /**
  * See https://www.w3.org/WAI/WCAG22/Techniques/general/G17.html for concrete details for WCAG 2.2.
+ *
+ * Note: this function is identical for WCAG 2.0 and 2.1 but 2.2 uses a different ratio constant.
  */
-export const luminance = (color: Color): number => {
-  const r = relativize(color.r);
-  const g = relativize(color.g);
-  const b = relativize(color.b);
+export const relativeLuminance = (value: number, wcag: WCAG = "2.2"): number => {
+  const ratio = value / 255;
+  if (wcag === "2.0" || wcag === "2.1") {
+    return ratio <= 0.03928 ? ratio / 12.92 : Math.pow((ratio + 0.055) / 1.055, 2.4);
+  }
+
+  return ratio <= 0.04045 ? ratio / 12.92 : Math.pow((ratio + 0.055) / 1.055, 2.4);
+};
+
+/**
+ * See https://www.w3.org/WAI/WCAG22/Techniques/general/G17.html for concrete details for WCAG 2.2.
+ *
+ * Note: this function is identical for WCAG 2.0, 2.1 and 2.2;
+ */
+export const luminance = (color: Color, wcag: WCAG = "2.2"): number => {
+  const r = relativeLuminance(color.r, wcag);
+  const g = relativeLuminance(color.g, wcag);
+  const b = relativeLuminance(color.b, wcag);
 
   return 0.2126 * r + 0.7152 * g + 0.0722 * b;
 };
 
 /**
  * See https://www.w3.org/WAI/WCAG22/Techniques/general/G17.html for concrete details for WCAG 2.2.
+ *
+ * Note: this function is identical for WCAG 2.0, 2.1 and 2.2;
  */
-export const contrast = (c1: Color, c2: Color = color({ r: 255, g: 255, b: 255 })): number => {
-  const l1 = luminance(c1);
-  const l2 = luminance(c2);
+export const contrast = (
+  c1: Color,
+  c2: Color = color({ r: 255, g: 255, b: 255 }),
+  wcag: WCAG = "2.2",
+): number => {
+  const l1 = luminance(c1, wcag);
+  const l2 = luminance(c2, wcag);
 
   return l1 > l2 ? (l1 + 0.05) / (l2 + 0.05) : (l2 + 0.05) / (l1 + 0.05);
 };
 
+/**
+ * This function determines whether two colors used together are readable based
+ * on WCAG readability criteria. The first color is the foreground color and the
+ * second the background color (defaulting to pure white, "#FFF" / [255, 255, 255]).
+ *
+ * The readability criteria defaults to the 'AAA' level for normal text as defined
+ * in WCAG 2.2.
+ *
+ * See https://www.w3.org/WAI/WCAG22/Techniques/general/G17.html for concrete details for WCAG 2.2.
+ */
 export const isReadable = (
-  c1: Color,
-  c2: Color = color({ r: 255, g: 255, b: 255 }),
-  readability: Readability = { level: "AAA", size: "normal" },
+  foreground: Color,
+  background: Color = color({ r: 255, g: 255, b: 255 }),
+  readability: Readability = { level: "AAA", size: "normal", wcag: "2.2" },
 ): boolean => {
-  const score = contrast(c1, c2);
+  const score = contrast(foreground, background, readability.wcag);
   return score >= contrastLevel(readability);
 };
 
+/**
+ * This function calculates the contrast and whether two colors used together are readable
+ * based on WCAG readability criteria. The first color is the foreground color and the
+ * second the background color (defaulting to pure white, "#FFF" / [255, 255, 255]).
+ *
+ * The readability criteria defaults to the 'AAA' level for normal text as defined
+ * in WCAG 2.2.
+ *
+ * See https://www.w3.org/WAI/WCAG22/Techniques/general/G17.html for concrete details for WCAG 2.2.
+ */
 export const calculateContrast = (
-  c1: Color,
-  c2: Color,
-  readability: Readability = { level: "AAA", size: "normal" },
+  foreground: Color,
+  background: Color,
+  readability: Readability = { level: "AAA", size: "normal", wcag: "2.2" },
 ): ReadabilityResult => {
-  const score = contrast(c1, c2);
+  const score = contrast(foreground, background, readability.wcag);
   const pass = score >= contrastLevel(readability);
 
   return {
@@ -70,12 +118,4 @@ export const calculateContrast = (
     score,
     pass,
   };
-};
-
-/**
- * See https://www.w3.org/WAI/WCAG22/Techniques/general/G17.html for concrete details for WCAG 2.2.
- */
-export const relativize = (value: number): number => {
-  const ratio = value / 255;
-  return ratio <= 0.04045 ? ratio / 12.92 : Math.pow((ratio + 0.055) / 1.055, 2.4);
 };
--- wcag_test.ts
+++ wcag_test.ts
@@ -1,5 +1,6 @@
 import { assert, assertEquals, assertFalse } from "testing/asserts.ts";
-import { contrast, hex, isReadable, luminance } from "./mod.ts";
+import { describe, it } from "testing/bdd.ts";
+import { contrast, hex, isReadable, luminance, Readability } from "./mod.ts";
 
 export const round = (number: number, digits = 2, base = Math.pow(10, digits)): number => {
   return Math.round((number + Number.EPSILON) * base) / base + 0;
@@ -9,39 +10,82 @@ export const floor = (number: number, digits = 2, base = Math.pow(10, digits)): 
   return Math.floor(number + Number.EPSILON * base) / base;
 };
 
-Deno.test("calculates perceived luminance of a color", () => {
-  assertEquals(luminance(hex("#000000")), 0);
-  assertEquals(round(luminance(hex("#e42189"))), 0.19);
-  assertEquals(round(luminance(hex("#ff0000"))), 0.21);
-  assertEquals(round(luminance(hex("#808080"))), 0.22);
-  assertEquals(round(luminance(hex("#aabbcc"))), 0.48);
-  assertEquals(round(luminance(hex("#ccddee"))), 0.71);
-  assertEquals(luminance(hex("#ffffff")), 1);
+describe("WCAG 2.2", () => {
+  it("calculates perceived luminance of a color", () => {
+    assertEquals(luminance(hex("#000000")), 0);
+    assertEquals(round(luminance(hex("#e42189")), 4), 0.1939);
+    assertEquals(round(luminance(hex("#ff0000")), 4), 0.2126);
+    assertEquals(round(luminance(hex("#808080")), 4), 0.2159);
+    assertEquals(round(luminance(hex("#aabbcc")), 4), 0.4845);
+    assertEquals(round(luminance(hex("#ccddee")), 4), 0.7072);
+    assertEquals(luminance(hex("#ffffff")), 1);
+  });
+
+  it("calculates a contrast ratio for a color pair", () => {
+    assertEquals(contrast(hex("#000000")), 21);
+    assertEquals(contrast(hex("#ffffff"), hex("#000000")), 21);
+    assertEquals(round(contrast(hex("#777777")), 4), 4.4781);
+    assertEquals(round(contrast(hex("#ff0000")), 4), 3.9985);
+    assertEquals(round(contrast(hex("#00ff00")), 4), 1.3722);
+    assertEquals(round(contrast(hex("#2e2e2e")), 4), 13.5798);
+    assertEquals(round(contrast(hex("#0079ad")), 4), 4.8405);
+    assertEquals(round(contrast(hex("#0079ad"), hex("#2e2e2e")), 4), 2.8054);
+    assertEquals(round(contrast(hex("#e42189"), hex("#0d0330")), 4), 4.5465);
+    assertEquals(round(contrast(hex("#fff4cc"), hex("#3a1209")), 4), 15.0067);
+  });
+
+  it("passes readability tests", () => {
+    assert(isReadable(hex("#000")));
+    assertFalse(isReadable(hex("#777777")));
+    assertFalse(isReadable(hex("#e60000"), hex("#ffff47")));
+    assertFalse(isReadable(hex("#af085c"), hex("#000000")));
+    assertFalse(isReadable(hex("#af085c"), hex("#000000"), { size: "large" }));
+    assertFalse(isReadable(hex("#d53987"), hex("#000000")));
+    assertFalse(isReadable(hex("#d53987"), hex("#000000"), { level: "AAA" }));
+    assert(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AA" }));
+    assertFalse(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AAA" }));
+    assert(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AAA", size: "large" }));
+    assert(isReadable(hex("#e9dddd"), hex("#67325e"), { level: "AAA" }));
+  });
 });
 
-Deno.test("calculates a contrast ratio for a color pair", () => {
-  assertEquals(contrast(hex("#000000")), 21);
-  assertEquals(contrast(hex("#ffffff"), hex("#000000")), 21);
-  assertEquals(round(contrast(hex("#777777"))), 4.48);
-  assertEquals(round(contrast(hex("#ff0000"))), 4);
-  assertEquals(round(contrast(hex("#00ff00"))), 1.37);
-  assertEquals(round(contrast(hex("#2e2e2e"))), 13.58);
-  assertEquals(round(contrast(hex("#0079ad"))), 4.84);
-  assertEquals(round(contrast(hex("#0079ad"), hex("#2e2e2e"))), 2.81);
-  assertEquals(round(contrast(hex("#e42189"), hex("#0d0330"))), 4.55);
-  assertEquals(round(contrast(hex("#fff4cc"), hex("#3a1209"))), 15.01);
-});
+describe("WCAG 2.0/2.1", () => {
+  const options: Readability = { wcag: "2.1" };
 
-Deno.test("passes readability tests", () => {
-  assert(isReadable(hex("#000")));
-  assertFalse(isReadable(hex("#777777")));
-  assertFalse(isReadable(hex("#e60000"), hex("#ffff47")));
-  assertFalse(isReadable(hex("#af085c"), hex("#000000")));
-  assertFalse(isReadable(hex("#af085c"), hex("#000000"), { size: "large" }));
-  assertFalse(isReadable(hex("#d53987"), hex("#000000")));
-  assertFalse(isReadable(hex("#d53987"), hex("#000000"), { level: "AAA" }));
-  assert(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AA" }));
-  assertFalse(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AAA" }));
-  assert(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AAA", size: "large" }));
-  assert(isReadable(hex("#e9dddd"), hex("#67325e"), { level: "AAA" }));
+  it("calculates perceived luminance of a color", () => {
+    assertEquals(luminance(hex("#000000"), options.wcag), 0);
+    assertEquals(round(luminance(hex("#e42189"), options.wcag), 4), 0.1939);
+    assertEquals(round(luminance(hex("#ff0000"), options.wcag), 4), 0.2126);
+    assertEquals(round(luminance(hex("#808080"), options.wcag), 4), 0.2159);
+    assertEquals(round(luminance(hex("#aabbcc"), options.wcag), 4), 0.4845);
+    assertEquals(round(luminance(hex("#ccddee"), options.wcag), 4), 0.7072);
+    assertEquals(luminance(hex("#ffffff"), options.wcag), 1);
+  });
+
+  it("calculates a contrast ratio for a color pair", () => {
+    assertEquals(contrast(hex("#000000"), hex("#fff"), options.wcag), 21);
+    assertEquals(contrast(hex("#ffffff"), hex("#000000"), options.wcag), 21);
+    assertEquals(round(contrast(hex("#777777"), hex("#fff"), options.wcag), 4), 4.4781);
+    assertEquals(round(contrast(hex("#ff0000"), hex("#fff"), options.wcag), 4), 3.9985);
+    assertEquals(round(contrast(hex("#00ff00"), hex("#fff"), options.wcag), 4), 1.3722);
+    assertEquals(round(contrast(hex("#2e2e2e"), hex("#fff"), options.wcag), 4), 13.5798);
+    assertEquals(round(contrast(hex("#0079ad"), hex("#fff"), options.wcag), 4), 4.8405);
+    assertEquals(round(contrast(hex("#0079ad"), hex("#2e2e2e"), options.wcag), 4), 2.8054);
+    assertEquals(round(contrast(hex("#e42189"), hex("#0d0330"), options.wcag), 4), 4.5465);
+    assertEquals(round(contrast(hex("#fff4cc"), hex("#3a1209"), options.wcag), 4), 15.0067);
+  });
+
+  it("passes readability tests", () => {
+    assert(isReadable(hex("#000")));
+    assertFalse(isReadable(hex("#777777")));
+    assertFalse(isReadable(hex("#e60000"), hex("#ffff47")));
+    assertFalse(isReadable(hex("#af085c"), hex("#000000")));
+    assertFalse(isReadable(hex("#af085c"), hex("#000000"), { size: "large" }));
+    assertFalse(isReadable(hex("#d53987"), hex("#000000")));
+    assertFalse(isReadable(hex("#d53987"), hex("#000000"), { level: "AAA" }));
+    assert(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AA" }));
+    assertFalse(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AAA" }));
+    assert(isReadable(hex("#e9dddd"), hex("#864b7c"), { level: "AAA", size: "large" }));
+    assert(isReadable(hex("#e9dddd"), hex("#67325e"), { level: "AAA" }));
+  });
 });
