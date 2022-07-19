e9b3e07d2f59e4f28a71c38ecb778baa5c9b1415
Sondre Aasemoen <sondre@eons.io>
2022-07-19 15:49:06 +0200

    Add documentation, example to 'toHex' function

--- color.ts
+++ color.ts
@@ -8,6 +8,18 @@ export const colorFromObject = (input: ColorObject): Color => {
 
 export const isValid = (color: Color): boolean => !Object.values(color).some(isNaN);
 
+/**
+ * Convert a RGB triplet into its hex string representation.
+ *
+ * See [this post](https://ryanclark.me/rgb-to-hex-via-binary-shifting/) for how.
+ *
+ * ```ts
+ * import { toHex } from "./color.ts";
+ *
+ * toHex([128, 128, 128])
+ * // "#808080"
+ * ```
+ */
 export const toHex = (color: Color): string => {
   const [r, g, b] = color;
   return "#" + (1 << 24 | (r << 16 | g << 8 | b)).toString(16).slice(1);
