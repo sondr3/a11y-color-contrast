0d20fe6effd652b2c5f630ecb59477a4a31e862b
Sondre Aasemoen <sondre@eons.io>
2022-07-19 15:46:50 +0200

    Change toHex method to use bit shifting dark magics

--- color.ts
+++ color.ts
@@ -6,16 +6,9 @@ export const colorFromObject = (input: ColorObject): Color => {
   return Object.values(input) as Color;
 };
 
-const prefixHexPart = (part: string): string => {
-  return part.length === 1 ? `0${part}` : part;
-};
-
 export const isValid = (color: Color): boolean => !Object.values(color).some(isNaN);
 
 export const toHex = (color: Color): string => {
-  const r = prefixHexPart(color[0].toString(16));
-  const g = prefixHexPart(color[1].toString(16));
-  const b = prefixHexPart(color[2].toString(16));
-
-  return `#${r}${g}${b}`;
+  const [r, g, b] = color;
+  return "#" + (1 << 24 | (r << 16 | g << 8 | b)).toString(16).slice(1);
 };
--- color_test.ts
+++ color_test.ts
@@ -5,4 +5,13 @@ Deno.test("can convert to hex", () => {
   assertEquals(toHex([255, 255, 255]), "#ffffff");
   assertEquals(toHex([170, 170, 170]), "#aaaaaa");
   assertEquals(toHex([128, 128, 128]), "#808080");
+  assertEquals(toHex([0, 0, 0]), "#000000");
+  assertEquals(toHex([65, 131, 196]), "#4183c4");
+  assertEquals(toHex([255, 154, 253]), "#ff9afd");
+  assertEquals(toHex([40, 42, 54]), "#282a36");
+  assertEquals(toHex([65, 131, 196]), "#4183c4");
+  assertEquals(toHex([255, 154, 253]), "#ff9afd");
+  assertEquals(toHex([160, 82, 45]), "#a0522d");
+  assertEquals(toHex([40, 42, 54]), "#282a36");
+  assertEquals(toHex([40, 42, 54]), "#282a36");
 });
